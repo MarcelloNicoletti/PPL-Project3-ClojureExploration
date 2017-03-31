5fb4eaaeb3b367d99ba91fed82818cd4b5fc5d60
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-03-30 19:21:48 -0600

    Correcting errors in project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject project3 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "PPL class project 3: Trying out clojure."
+  :url "https://gitlab.com/CupricWolf/PPL-Project3-Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot :project3.core
+  :main ^:skip-aot project3.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
   )
